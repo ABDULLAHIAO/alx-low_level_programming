8fbfcc44dfeaec452807aee927fcc1f009a4c068
Abdullahi_OO <oyelakinabdullahioyedele@gmail.com>
2022-06-05 21:55:52 +0100

    Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,8 +13,8 @@ int main(void)
 	printf("The size of an int is: %zu.\n", sizeof(int));
 	printf("The size of an int in a 8-bit manchine: %lu.\n", sizeof( int8_t ));
 	printf("The size of a signed integer in a 16-bit manchine is: %lu.\n", sizeof( int16_t ));
-	puts("The size of an unsigned int in a 32-bit manchine is: %zu.\n", sizeof(uint32_t));
-	puts("The size of an unsigned int in a 64-bit manchine is: %lu.\n", sizeof(uint64_t));
+	printf("The size of an unsigned int in a 32-bit manchine is: %zu.\n", sizeof(uint32_t));
+	printf("The size of an unsigned int in a 64-bit manchine is: %lu.\n", sizeof(uint64_t));
 	printf("The size of a short int is: %zu.\n", sizeof(short int));
 	printf("The size of a long int is: %zu.\n", sizeof(long int));
 	printf("The size of a char is: %zu.\n", sizeof(char));
