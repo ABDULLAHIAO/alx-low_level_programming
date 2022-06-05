0c6df741812a118ff2652190ddb7e131bc30924f
Abdullahi_OO <oyelakinabdullahioyedele@gmail.com>
2022-06-05 20:52:28 +0100

    Size of grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,9 +12,9 @@ int main(void)
 	printf("The size of a short int is: %i.\n", sizeof(short int));
 	printf("The size of a long int is: %lu.\n", size of(long int));
 	printf("The size of a char is: %lu.\n", sizeof(char));
-	printf("The size of a signed integer is: %lu.\n", size of(signed int));
-	printf("The size of an unsigned integer is: %lu.\n", size of(unsigned int));
-	printf("The size of a double is: %lu.\n", size of(double));
-	printf("The size of a float is: %lu.\n", size of(float));
+	printf("The size of a signed integer is: %lu.\n", sizeof(signed int));
+	printf("The size of an unsigned integer is: %lu.\n", sizeof(unsigned int));
+	printf("The size of a double is: %lu.\n", sizeof(double));
+	printf("The size of a float is: %lu.\n", sizeof(float));
 	return (0);
 }
