90234a9d7b1b6cb16835dff8686ec63f6e2ff9f7
Abdullahi_OO <oyelakinabdullahioyedele@gmail.com>
2022-06-05 22:16:06 +0100

    Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,17 +10,18 @@
 
 int main(void)
 {
-	printf("The size of an int is: %zu.\n", sizeof(int));
-	printf("The size of an int in a 8-bit manchine: %lu.\n", sizeof( int8_t ));
-	printf("The size of a signed integer in a 16-bit manchine is: %lu.\n", sizeof( int16_t ));
-	printf("The size of an unsigned int in a 32-bit manchine is: %zu.\n", sizeof(uint32_t));
-	printf("The size of an unsigned int in a 64-bit manchine is: %lu.\n", sizeof(uint64_t));
-	printf("The size of a short int is: %zu.\n", sizeof(short int));
-	printf("The size of a long int is: %zu.\n", sizeof(long int));
-	printf("The size of a char is: %zu.\n", sizeof(char));
-	printf("The size of a signed integer is: %zu.\n", sizeof(signed int));
-	printf("The size of an unsigned integer is: %zu.\n", sizeof(unsigned int));
-	printf("The size of a double is: %zu.\n", sizeof(double));
-	printf("The size of a float is: %zu.\n", sizeof(float));
+	printf("The size of an int is: %zu byte(s)\n", sizeof(int));
+	printf("The size of an int in a 8-bit manchine: %lu byte(s)\n", sizeof( int8_t ));
+	printf("The size of a signed integer in a 16-bit manchine is: %lu byte(s)\n", sizeof( int16_t ));
+	printf("The size of an unsigned int in a 32-bit manchine is: %zu byte(s)\n", sizeof(uint32_t));
+	printf("The size of an unsigned int in a 64-bit manchine is: %lu byte(s)\n", sizeof(uint64_t));
+	printf("The size of a short int is: %zu byte(s)\n", sizeof(short int));
+	printf("The size of a long int is: %zu byte(s) \n", sizeof(long int));
+	printf("The size of a long long int is: %zu byte(s)\n", sizeof(long long int));
+	printf("The size of a char is: %zu byte(s)\n", sizeof(char));
+	printf("The size of a signed integer is: %zu byte(s)\n", sizeof(signed int));
+	printf("The size of an unsigned integer is: %zu byte(s)\n", sizeof(unsigned int));
+	printf("The size of a double is: %zu byte(s)\n", sizeof(double));
+	printf("The size of a float is: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
