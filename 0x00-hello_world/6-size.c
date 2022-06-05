bcb2bdcf7258514ed7421056b3a68417c8afcc32
Abdullahi_OO <oyelakinabdullahioyedele@gmail.com>
2022-06-05 20:08:23 +0100

    Size is not grandeur

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * return - program returns 0 always
+ * printf - delivers message to the standard output
+ */
+
+int main(void)
+{
+	printf("The size of an int is: %d.\n", sizeof(int));
+	printf("The size of a short int is: %i.\n", sizeof(short int));
+	printf("The size of a long int is: %lu.\n", size of(long int));
+	printf("The size of a char is: %lu.\n", sizeof(char));
+	printf("The size of a signed integer is: %lu.\n", size of(signed int));
+	printf("The size of an unsigned integer is: %lu.\n", size of(unsigned int));
+	printf("The size of a double is: %lu.\n", size of(double));
+	printf("The size of a float is: %lu.\n", size of(float));
+	return (0);
+}
