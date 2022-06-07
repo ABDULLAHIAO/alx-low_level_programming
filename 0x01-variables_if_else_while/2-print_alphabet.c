cbdcb6224cc61f063fb53f81a89331816f4650a7
Abdullahi_OO <oyelakinabdullahioyedele@gmail.com>
2022-06-07 21:44:08 +0100

    Lowercase Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+char Alphabets;
+
+int main()
+{
+	void lowercase_ascii
+	{
+		//printing lowercase alphabets using ASCII 
+		for (Alphabets = 97; Alphabets <= 122; Alphabets++)
+		{
+			printf("%c\t\n", Alphabets);
+		}
+	}
+	
+	void lowercase_letters
+	{
+		//printing lowercase letters using alphabet characters itself
+		for (Alphabets = a; Alphabets <= z; Alphabets++)
+		{
+			printf("%c\t\n", Alphabets);
+		}
+	}
+}
