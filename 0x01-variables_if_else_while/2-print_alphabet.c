137e71e6f6a812c2d50702476991a2428f41a24c
Abdullahi_OO <oyelakinabdullahioyedele@gmail.com>
2022-06-07 22:18:01 +0100

    Lowercase Alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,17 +6,15 @@
  * Return: Always 0 (Success)
  */
 
-
+int main()
+{
 	char Alphabets;
 
 		/*printing lowercase alphabets using ASCII*/
+
 		for (Alphabets = 97; Alphabets <= 122; Alphabets++)
 		{
 			putchar("%c\t\n", Alphabets);
 		}
 	}
-
-	int main()
-{
-	putchar("%c\t\n", Alphabets);
 }
