058aadc196f42a1305961557108dce0a0867a6ac
Abdullahi_OO <oyelakinabdullahioyedele@gmail.com>
2022-06-08 14:04:55 +0100

    Alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int Alphabet = 'a';
+	
+	while (Alphabet <= 'z')
+	{
+		if (Alphabet != e && Alphabet != q)
+		{
+			putchar(Alphabet);
+		}
+
+		++Alphabet;
+		putchar('\n');
+	}
+	return(0);
+}
+	
+
