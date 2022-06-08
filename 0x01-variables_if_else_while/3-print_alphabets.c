c25b7f40df8f62db209c9303cc44e2f65d5227ef
Abdullahi_OO <oyelakinabdullahioyedele@gmail.com>
2022-06-08 12:27:38 +0100

    alphaBETS

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,16 +8,16 @@
 
 int main(void)
 {
-	char alpha;
+	int alpha;
 
 	for (alpha = 'a'; alpha <= 'z'; ++alpha)
 	{
-		putchar("alpha");
+		putchar(alpha);
 	}
 
 	for (alpha = 'A'; alpha <= 'Z'; ++alpha)
 	{
-		putchar("alpha");
+		putchar(alpha);
 	}
 
 	putchar('\n');
