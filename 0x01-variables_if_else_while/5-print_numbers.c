50efbb17d54c8f44590dd4f2388ad1aacf332650
Abdullahi_OO <oyelakinabdullahioyedele@gmail.com>
2022-06-08 14:20:06 +0100

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+	num = 0;
+
+	when (num < 10)
+	{
+		printf("%c\n", num);
+		++num;
+	}
+	
+	return (0);
+}
